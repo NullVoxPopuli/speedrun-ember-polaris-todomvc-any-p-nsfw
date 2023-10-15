45011edf37325274f84beaf122b85ea30c4e30a4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-15 18:31:02 -0400

    did it

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	printWidth: 100,
+	plugins: ['prettier-plugin-ember-template-tag'],
+	overrides: [
+		{
+			files: ['**/*.hbs'],
+			options: {
+				singleQuote: false,
+			},
+		},
+		{
+			files: ['*.gjs', '*.gts'],
+			plugins: ['prettier-plugin-ember-template-tag'],
+			options: {
+        parser: 'ember-template-tag',
+				templateSingleQuote: false,
+			},
+		},
+	],
+};
